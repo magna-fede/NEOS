9863f6bae92d071047ba34532e275bcb80b970a6
Federica Magnabosco <magna.federica@gmail.com>
2023-05-11 18:46:46 +0100

    included snr computations for overweighting comparison

--- plot_conditionoverweighting_procedure.py
+++ plot_conditionoverweighting_procedure.py
@@ -37,11 +37,16 @@ flat_criteria = config.epo_flat
 def plot_evoked_for_comparisons(sbj_id):
     sbj_path = path.join(config.data_path, config.map_subjects[sbj_id][0])
     
-    for condition in ['eog', 'variance', 'both']:
-        for over in ['_ovrw', '', '_ovrwonset']:
-            raws = apply_ica.get_ica_raw(sbj_id, condition=condition,
-                                         overweighting=over)
 
+    for condition in ['eog', 'variance', 'both']:
+        snr_ica = dict()
+        for key, over in zip(["nover", "ovrw", "ovrwonset"], ['', '_ovrw', '_ovrwonset']):
+            raws = apply_ica.get_ica_raw(sbj_id, condition=condition,
+                                         overweighting=over, drop_EEG_4_8=False)
+            evts = mne.read_events(path.join(sbj_path, config.map_subjects[sbj_id][0][-3:] + \
+                      '_all_events.fif'))
+            snr_ica[key] = snr_metrics.compute_metrics(raws.copy(), evts, plot=False)
+            
             target_evts = mne.read_events(path.join(sbj_path, config.map_subjects[sbj_id][0][-3:] + \
                       '_target_events.fif'))
     
@@ -60,4 +65,34 @@ def plot_evoked_for_comparisons(sbj_id):
                 fname_fig = path.join(sbj_path, 'Figures', f'FRP_all_{i}_{condition}{over}.png')
                 fig.savefig(fname_fig)
             
+        df = pd.DataFrame(snr_ica).T
+        cols = {
+            "P1_SNR": "P1 SNR",
+            "GFP_first100": "GFP (first 100 ms)",
+            "GFP_baseline": "GFP (Baseline)",
+            "GFP_fixation_onset": "GFP (Fixation Onset)",
+            "GFP_late": "GFP (last 300 ms)",
+            "S_amplitude": "Saccade Peak Ampl.",
+            "S_auc": "Saccade AUC",
+        }
+        order = ["nover", "ovrw", "ovrwonset"]
+        palette = dict(zip(order, ["dodgerblue","yellowgreen", "plum"]))
+        
+        fig, axx = plt.subplots(1, len(cols), figsize=(20, 5), tight_layout=True)
+        
+        for m, col in enumerate(cols.keys()):
+        
+            ax = axx[m]
+            sns.barplot(data=df, x=["nover", "ovrw", "ovrwonset"], y=col, ax=ax, order=order, palette=palette)
+            ax.set_title(cols[col])
+            ax.set_xlabel("")
+            ax.set_ylabel("")
+        
+        sns.despine(fig)
+        fname_fig = path.join(sbj_path, 'Figures', f'snr_metrics_ICA_componentselection_{sbj_id}_{condition}.png')
+        fig.savefig(fname_fig) 
+           
+           
+        df.to_csv(path.join(sbj_path, f"snr_compareoverweight_{sbj_id}_{condition}.csv"))
+
         
