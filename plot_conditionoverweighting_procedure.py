92c97f7da9d9e54742ddb0ca27cc82d92aa4ffa2
Federica Magnabosco <magna.federica@gmail.com>
2023-05-11 12:32:41 +0100

    made the script simpler

--- plot_conditionoverweighting_procedure.py
+++ plot_conditionoverweighting_procedure.py
@@ -37,29 +37,16 @@ flat_criteria = config.epo_flat
 def plot_evoked_for_comparisons(sbj_id):
     sbj_path = path.join(config.data_path, config.map_subjects[sbj_id][0])
     
-    raws = dict()
-    
     for condition in ['eog', 'var', 'both']:
-        raws[condition] = dict()        
-        for key in [
-                    'ovrw',
-                    'novr',
-                     'ovrwonset'
-                    ]:
-            raws[condition][key] = list()
-        
-            for key, over in zip(raws[condition].keys(), ['_ovrw', '', '_ovrwonset']):
-                raws[condition][key] = apply_ica.get_ica_raw(sbj_id, condition=condition,
-                                                             overweighting=over)
-    
-    target_evts = mne.read_events(path.join(sbj_path, config.map_subjects[sbj_id][0][-3:] + \
+        for over in ['_ovrw', '', '_ovrwonset']:
+            raws = apply_ica.get_ica_raw(sbj_id, condition=condition,
+                                                         overweighting=over)
+
+            target_evts = mne.read_events(path.join(sbj_path, config.map_subjects[sbj_id][0][-3:] + \
                       '_target_events.fif'))
     
-    for condition in ['eog', 'var', 'both']:
-        for key, over in zip(raws[condition].keys(), ['_ovrw', '', '_ovrwonset']):
-            
             event_dict = {'FRP': 999}
-            epochs = mne.Epochs(raws[condition][key], target_evts, picks=['meg', 'eeg', 'eog'],
+            epochs = mne.Epochs(raws, target_evts, picks=['meg', 'eeg', 'eog'],
                                 tmin=-0.3, tmax=0.7, 
                                 event_id=event_dict,    
                                 reject=reject_criteria, flat=flat_criteria,
@@ -72,5 +59,5 @@ def plot_evoked_for_comparisons(sbj_id):
             for i, fig in zip(['EEG','MAG','GRAD'], FRP_fig):
                 fname_fig = path.join(sbj_path, 'Figures', f'FRP_all_{i}_{condition}{over}.png')
                 fig.savefig(fname_fig)
-                
+            
         
