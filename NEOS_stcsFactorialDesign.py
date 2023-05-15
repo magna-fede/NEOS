d56c9df0ef5a3a4830cdb4df20deb1d384ca6da2
Federica Magnabosco <magna.federica@gmail.com>
2023-05-15 17:55:57 +0100

    added script for computeing stcs when dropbads condition

--- NEOS_stcsFactorialDesign.py
+++ NEOS_stcsFactorialDesign.py
@@ -58,6 +58,37 @@ def compute_stcs(sbj_id, method="MNE", inv_suf='', orientation=None):
         elif len(inv_suf)>0:
             stc_fname = path.join(stc_path, f"{subject}_stc_{ev}_{method}_{inv_suf}")
         stc.save(stc_fname)
+        
+def compute_stcs_dropbads(sbj_id, method="MNE", inv_suf='', orientation=None):
+    subject = str(sbj_id)
+    sbj_path = path.join(config.data_path, config.map_subjects[sbj_id][0])
+    inv_fname = path.join(sbj_path, subject + f'_EEGMEG{inv_suf}-inv.fif')
+    inverse_operator = mne.minimum_norm.read_inverse_operator(inv_fname)
+    evs=dict()
+    for condition in conditions:
+        fname = path.join(ave_path, f"{subject}_evokeds_dropbads.fif")
+        evokeds = mne.read_evokeds(fname)
+        ev_0 = list()
+        ev_1 = list()
+        for e, evoked in enumerate(evokeds):
+            if re.match(f'([a-z]*\/)?{condition[0]}(\/[a-z]*)?', evoked.comment, re.IGNORECASE):
+                ev_0.append(evoked)
+            elif re.match(f'([a-z]*\/)?{condition[1]}(\/[a-z]*)?', evoked.comment, re.IGNORECASE):
+                ev_1.append(evoked)  
+                
+        evs[condition[0]] = mne.combine_evoked(ev_0, weights='nave')
+        evs[condition[1]] = mne.combine_evoked(ev_1, weights='nave')
+        
+    for ev in evs.keys():
+        stc = mne.minimum_norm.apply_inverse(evs[ev], inverse_operator,
+                                             lambda2, method=method,
+                                             pick_ori=orientation, verbose=True)
+        if len(inv_suf)==0:
+            stc_fname = path.join(stc_path, f"{subject}_stc_{ev}_{method}")
+        elif len(inv_suf)>0:
+            stc_fname = path.join(stc_path, f"{subject}_stc_{ev}_{method}_{inv_suf}")
+        stc.save(stc_fname)
+
 
 # if len(sys.argv) == 1:
 
