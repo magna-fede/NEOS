bacd0036ac2644ca1c61c78dce6ada552070acb2
Federica Magnabosco <magna.federica@gmail.com>
2023-05-19 16:19:39 +0100

    added function for computing decoding with (binned) continuous variables

--- NEOS_MakeStcsROIs_SingleEpochs_decoding.py
+++ NEOS_MakeStcsROIs_SingleEpochs_decoding.py
@@ -11,7 +11,7 @@ from os import path
 
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, KBinsDiscretizer
 
 import pickle
 
@@ -91,11 +91,105 @@ rois = [lATL,
         PTC]
 
 meta = pd.read_csv('/imaging/hauk/users/fm02/MEG_NEOS/stim/meg_metadata.csv', header=0)
-pred = ['ID', 'Word', 'ConcM', 'LEN', 'LogFreq(Zipf)', 'Position', 'Sim']
+pred = ['ConcM', 'LEN', 'LogFreq(Zipf)', 'Position', 'Sim']
 meta = meta[pred]
 
+est = KBinsDiscretizer(n_bins=4, encode='ordinal', strategy='uniform')
+est.fit(meta)
+Xt = est.transform(meta)
+
+cols = ['ConcCont', 'Length', 'Zipf', 'Position', 'PredCont']
 
 # %%
+
+def decoding_continuous_predictors(sbj_id, method='eLORETA', inv_suf='shrunk_dropbads'):
+    
+    subject = str(sbj_id)
+    sbj_path = path.join(config.data_path, config.map_subjects[sbj_id][0])
+    bad_eeg = config.bad_channels_all[sbj_id]['eeg']
+    
+    ovr = config.ovr_procedure[sbj_id]
+    ovr = ovr_sub(ovr)
+
+    raw_test = apply_ica.get_ica_raw(sbj_id, 
+                                     condition='both',
+                                     overweighting=ovr,
+                                     interpolate=False, 
+                                     drop_EEG_4_8=False)
+    
+    raw_test = raw_test.set_eeg_reference(ref_channels='average', projection=True)
+    raw_test.load_data()
+    raw_test.info['bads'] = bad_eeg
+    
+    picks = mne.pick_types(raw_test.info, meg=True, eeg=True, exclude='bads')
+
+    target_evts = mne.read_events(path.join(sbj_path, config.map_subjects[sbj_id][0][-3:] + \
+                             '_target_events.fif'))
+     
+    rows = np.where(target_evts[:,2]==999)[0]
+     
+    event_dict = {'FRP': 999}
+     
+    tmin, tmax = -.3, .7
+       
+    epochs = mne.Epochs(raw_test, target_evts, event_dict, tmin=tmin, tmax=tmax,
+                       picks=picks, reject=None, preload=True)
+     
+    metadata = pd.DataFrame(columns=meta.columns)
+     
+    for row in rows: 
+        index = target_evts[row-2, 2]*100 + target_evts[row-1, 2]
+        metadata = pd.concat([metadata,
+                             meta[meta['ID']==index]])
+     
+    epochs.metadata = metadata
+    
+    y = Xt[metadata.index,:]
+    
+    epochs.resample(250, npad='auto')
+    
+    sbj_path = path.join(config.data_path, config.map_subjects[sbj_id][0])
+    inv_fname = path.join(sbj_path, subject + f'_EEGMEG{inv_suf}-inv.fif')
+    inverse_operator = mne.minimum_norm.read_inverse_operator(inv_fname)
+    
+    rois_subject = mne.morph_labels(rois, subject_to=subject, 
+                                    subject_from='fsaverage', 
+                                    subjects_dir=config.subjects_dir)
+  
+    rois_lab = ['lATL',
+                  'rATL', 
+                  'PVA',
+                  'IFG',
+                  'AG',
+                  'PTC']
+        
+    stc = dict()
+    X = dict()
+    for roi, key in zip(rois_subject, rois_lab):
+        stc[key] = apply_inverse_epochs(epochs, inverse_operator, lambda2, method, label=roi,
+                                pick_ori="normal", nave=len(epochs))
+        X[key] = np.array([s.data for s in stc[key]])
+
+
+     # prepare a series of classifier applied at each time sample
+    clf = make_pipeline(
+        StandardScaler(),  # z-score normalization
+        LinearModel(LogisticRegression(C=1, solver='lbfgs',
+                                       max_iter=1000)),
+    )
+    time_decod = SlidingEstimator(clf, scoring='roc_auc_ovr')
+    
+    # Run cross-validated decoding analyses:
+
+    scores = dict.fromkeys(rois_lab)
+    for i, cond in enumerate(cols):
+        for roi in rois_lab:
+            y_cond = y[:,i]
+            scores[roi] = cross_val_multiscore(time_decod, X[roi], y_cond, cv=5, n_jobs=None)
+        
+        with open(f'/imaging/hauk/users/fm02/MEG_NEOS/data/Decoding/source_space/{sbj_id}_scores_{cond}.P', 'wb') as handle:
+            pickle.dump(scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 def get_decoding_scores(sbj_id, method='eLORETA', inv_suf='shrunk_dropbads'):
     
@@ -192,6 +286,7 @@ def get_decoding_scores(sbj_id, method='eLORETA', inv_suf='shrunk_dropbads'):
         
         with open(f'/imaging/hauk/users/fm02/MEG_NEOS/data/Decoding/source_space/{sbj_id}_scores_{cond}.P', 'wb') as handle:
             pickle.dump(scores, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
             
 # if len(sys.argv) == 1:
 
